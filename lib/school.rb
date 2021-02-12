efdddcceaa53b062813a59bec492f8d4775707fe
Abner <abnercabrera28@gmail.com>
2021-02-12 14:39:12 -0500

    commit tree 432727e3e12ee7c9ff73de0d335eacf2d9b0f6c1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    attr_accessor :roster, :add_student
+
+    def add_student(student, grade)
+        @student = student
+        @grade = grade
+        roster[grade] ||= []
+        roster[grade] << student
+        
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each do |grade, students|
+            students.sort!
+        end
+    end
+
+
+end
